9611a6da2cf551a309b058deafa6b9728305a425
tama <yuki023201025@gmail.com>
2020-01-20 20:38:58 +0900

    lesson7の2問目の確認問題のinputがわからない

---
+++ ruby確認問題/lesson7_2.rb
@@ -0,0 +1,25 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+
+l = gets.to_i
+
+while do
+
+    puts "#{l}回目の計算"
+    puts "2つの値を入力して下さい"
+
+    a = gets.to_i
+    b = gets.to_i
+
+    puts "a=#{a}"
+    puts "b=#{b}"
+
+    puts "計算結果を出力します"
+
+    puts "a+b=#{a + b}"
+    puts "a-b=#{a - b}"
+    puts "a*b=#{a * b}"
+    puts "a/b=#{a / b}"
+end
+
+puts = "計算を終了します"
